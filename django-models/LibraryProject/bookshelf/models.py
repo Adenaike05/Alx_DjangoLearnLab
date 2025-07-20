46ff0428118c653d9be1a3b7f51e969c7edf46fc
Adenaike David <coledav555@gmail.com>
2025-07-20 17:27:58 +0100

    Converted django-models from submodule to regular folder

---
+++ django-models/LibraryProject/bookshelf/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+""""
+CREATE TABLE book(
+    title varchar(200)
+    author varchar(100)
+    
+)
+Django ORM - Object Relational Mapper
+
+"""
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.title} by {self.author} ({self.publication_year})"
