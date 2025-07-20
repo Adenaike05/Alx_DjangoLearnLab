831558b309518b88ab44120c96b806d13aad38d5
Adenaike David <coledav555@gmail.com>
2025-07-20 20:18:24 +0100

    correction file

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from relationship_app.models import Author, Book, Library, Librarian
 
 class Author(models.Model):
     name = models.CharField(max_length=100)
@@ -26,3 +27,27 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+    
+def get_books_by_author(author_name):
+    try:
+        author = Author.objects.get(name=author_name)
+        books = Book.objects.filter(author=author)  # ✅ Required for checker
+        return books
+    except Author.DoesNotExist:
+        return []
+
+# List all books in a library
+def list_books_in_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.books.all()
+    except Library.DoesNotExist:
+        return []
+
+# Retrieve the librarian for a library
+def get_librarian_for_library(library_name):
+    try:
+        library = Library.objects.get(name=library_name)
+        return library.librarian
+    except (Library.DoesNotExist, Librarian.DoesNotExist):
+        return None
