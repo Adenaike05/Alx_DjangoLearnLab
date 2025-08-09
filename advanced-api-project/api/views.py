dfeb4e09022795abe8e02ad0286447833dfd345d
Adenaike David <coledav555@gmail.com>
2025-08-09 04:26:53 +0100

    view.py import

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
